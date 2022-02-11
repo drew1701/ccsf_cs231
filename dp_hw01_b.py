409080205d13c5646df172a2913246cf9ba6f085
drew1701 <drew@drewpatrick.org>
2022-02-10 16:29:47 -0800

    hw01 works
    produces correct results
    next: check multiple paths on open

--- dp_hw01_b.py
+++ dp_hw01_b.py
@@ -45,26 +45,27 @@ def check_all_pairs(this_line):
 def find_palindromes(test_file):
     for line in test_file:
         line = line.strip()
+        print(line)
         if len(line) < 1:
             continue
         elif len(line) == 1:
+            print('adding to trivial = ' + line)
             increment_trivial(palindrome)
         elif len(line) == 2:
             if is_pair_same(line, 0):
                 increment_trivial(palindrome)
+                print('adding to trivial = ' + line)
             else:
                 continue
         elif check_all_pairs(line):
+            print('adding to strict = ' + line)
             increment_strict(palindrome)
+        # print('end of for loop')
 
 
+# Call Functions
 find_palindromes(safe_open(R_PATH))
 
-print('\nA palindrome is a word that reads the same forward and backward.')
-print('Palindromes of <3 characters are sometimes called trivial.')
-print('The file ' + '/users/abrick/resources/english')
-print('has a total of ' + str(sum(palindrome.values())) + ' palindromes.')
-print(str(palindrome['trivial']) + ' are trivial in length, ' +
-      str(palindrome['strict']) + ' are at lest 3 characters.\n')
-if check_all_pairs(str(sum(palindrome.values()))):
-    print('The total number of palindromes is a palindrome.\n')
+print('A palindrome is a word that reads the same forward and backward.')
+print('Palindromes of less than 3 characters are considered "trivial" \n'
+      'in some circles')
--- testy.py
+++ testy.py
@@ -6,4 +6,34 @@ Homework XX: Task:
  Write a program that .
 """
 
-print('hello world')
+print('this is testy')
+
+test_lst = [
+    'a',
+    'bb',
+    'not',
+    'abcba',
+    'aabbccdccbbaa',
+    'stillnot',
+    '',
+    '12321'
+]  # test_lst has: trival 2, strict 3
+
+PATH = "test_hw01.txt"
+
+def safe_open(path):
+    with open(path, 'r') as file_reader:
+        return file_reader.readlines()
+
+
+for line in safe_open(PATH):
+    line = line.strip()
+    if len(line) < 1:
+        print(line + ' is 0 characters')
+        continue
+    elif len(line) == 1:
+        print (line + ' is 1 character')
+    elif len(line) == 2:
+        print (line + ' is 2 characters')
+    else:
+        print (line + ' is 3 or more characters')
