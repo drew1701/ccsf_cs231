b09b3a8551753fa061ea8e18cc35c891e7a570e0
Scott Mefferd <sm1@digimountain.com>
2022-02-11 10:43:59 -0700

    Added unit test

---
+++ dp_hw01_test.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+import dp_hw01
+
+
+class dp_hw01_Test(TestCase):
+    def test_check_all_pairs(self):
+        palindromes = [
+            'a',
+            'bb',
+            'abcba',
+            'aabbccdccbbaa',
+            '',
+            '12321'
+        ]
+        not_palindromes = [
+            'not',
+            'stillnot',
+        ]
+        for test_str in palindromes:
+            self.assertTrue(dp_hw01.check_all_pairs(test_str), test_str)
+        for test_str in not_palindromes:
+            self.assertFalse(dp_hw01.check_all_pairs(test_str), test_str)
