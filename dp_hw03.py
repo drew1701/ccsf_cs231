c008f92ccee94e7a2f1903a3478d096a300393a5
drew1701 <drew@drewpatrick.org>
2022-02-26 10:00:54 -0800

    hw03 generator func working
    convert to generator expression next

--- dp_hw03.py
+++ dp_hw03.py
@@ -9,15 +9,21 @@ Homework 03: Task:
 
 # pi = (4/1 - 4/3)  + (4/5 - 4/7) + 4/9 - 4/11 ....
 
+
 def my_pi(max_repeats):
     res = 0
     sign = 1
     N = 1
-    for i in range (max_repeats):
+    print('start')
+    for i in range(max_repeats):
         res += 4 * sign / N
+#        print(res)
         yield res
         sign *= -1
         N += 2
 
 
-print(my_pi(5))
+print('hllo')
+# my_pi(100)
+for x in my_pi(100):
+    print(x)
